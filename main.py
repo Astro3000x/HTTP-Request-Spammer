d0e396e794aaec3e51569b57d40ec0c6c8816796
Astro3000x <94796723+Astro3000x@users.noreply.github.com>
2022-01-20 21:06:51 -0800

    main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import socket
+# create an INET, STREAMing socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("What website would you like to HTTP request spam?")
+site = input()
+bob = socket.gethostbyname(site)
+# now connect to the web server on port 80 - the normal http port
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# bind the socket to a public host, and a well-known port
+i = 0
+
+while 1 == 1:
+
+    # become a server socket
+    #serversocket.listen(5)
+  print((socket.gethostbyname(site), 80))
+  print((socket.gethostbyname(site), 80))
+
+
+  print((socket.gethostbyname(site), 80))
+  print((socket.gethostbyname(site), 80))
+  print((socket.gethostbyname(site), 80))
+  print((socket.gethostbyname(site), 80))
+  print((socket.gethostbyname(site), 80))
+  print((socket.gethostbyname(site), 80))
+  print((socket.gethostbyname(site), 80))
+  print((socket.gethostbyname(site), 80))
+  print((socket.gethostbyname(site), 80))
+  i += 11
+  print(i)
+  s.close()
